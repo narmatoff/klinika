d63324afe7fb3c1b28bfe53783ae63faa8ea2c4c
narmatoff <narmatoff@ya.ru>
2015-10-22 20:46:22 +0300

    2210152046

--- src/js_src/main.js
+++ src/js_src/main.js
@@ -1,16 +1,38 @@
 $(document).ready(function() {
 
     // fancybox
-    $(".fancybox").fancybox();
+    // $(".fancybox").fancybox();
 
     // flexsliders
-    $('.firsttype_slider_wrapper').flexslider({
+    $('.firsttype_slider').flexslider({
         animation: "slide",
         animationLoop: false,
         itemWidth: 316,
         itemMargin: 0,
-        minItems: 3,
-        maxItems: 3
+        // minItems: 3,
+        // maxItems: 3,
+        controlsContainer: $(".custom-controls-container"),
+        customDirectionNav: $(".custom-navigation a")
+    });
+
+    $('.gallerytype_slider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        // itemWidth: 316,
+        maxItems: 1,
+        itemMargin: 0,
+        controlsContainer: $(".custom-controls-container2"),
+        customDirectionNav: $(".custom-navigation2 a")
+    });
+    $('.doctortype_slider').flexslider({
+        animation: "slide",
+        animationLoop: false,
+        itemWidth: 298,
+        itemMargin: 0,
+        // minItems: 3,
+        // maxItems: 3,
+        controlsContainer: $(".custom-controls-container3"),
+        customDirectionNav: $(".custom-navigation3 a")
     });
 
 });
