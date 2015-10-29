d8b19728a63e355cb67191b640dab47e747bab9f
narmatoff <narmatoff@gmail.com>
2015-10-29 17:13:13 +0300

    291020151703

---
+++ src/js_src/main.js
@@ -0,0 +1,169 @@
+$(document).ready(function() {
+
+    // menu on henhelds
+    $(".hamburger").click(function() {
+        $(".topmenu").show('fast');
+    });
+
+    $(".menu_sloser").click(function() {
+        $(".topmenu").hide('fast');
+    });
+
+    // избегаем скрытие topmenu в desktop версии после его скрытия в hendhelds
+    function windowSize() {
+        if ($(window).width() <= '620') {
+            $('.topmenu').hide();
+        } else {
+            $('.topmenu').show();
+        }
+    }
+    $(window).on('load resize', windowSize);
+
+
+
+
+    // fancybox
+    $(".fancybox").fancybox();
+    // слайдер услуг
+    $('.firsttype_slider>.slides').slick({
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        // centerMode: true,
+        variableWidth: true,
+        autoplay: false,
+        responsive: [{
+                breakpoint: 1150,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            }, {
+                breakpoint: 775,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            }, {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            }, {
+                breakpoint: 620,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false
+
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+    // слайдер галереи
+    $('.gallerytype_slider>.slides').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        responsive: [{
+            breakpoint: 1150,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 775,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 700,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 620,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                dots: true
+
+            }
+        }]
+
+    });
+
+
+    // слайдер врачей
+    $('.doctortype_slider>.slides').slick({
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        // centerMode: true,
+        variableWidth: true,
+        autoplay: false,
+        responsive: [{
+                breakpoint: 1150,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            }, {
+                breakpoint: 775,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            }, {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            }, {
+                breakpoint: 620,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false
+
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+});
